93474c57d4fbc9b973e3ce6d01cb7aaa57093808
m1raclemax <hpscigeek@gmail.com>
2026-03-22 22:50:24 -0400

    rush job but vaguely passable

--- app/page.js
+++ app/page.js
@@ -5,9 +5,17 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1>COMMBADGE</h1>
-        <h3>Ever wanted to see what's going on in those chat apps everyone uses? You probably know some of their names, maybe even some strategies they use, but what if you could make one?</h3>
-        <h2>Well, why don't you? </h2>
+        <h1 className={styles.header}>COMMBADGE</h1>
+        <h3>
+          Ever wanted to see what's going on in those chat apps everyone uses? You probably know some of their names, maybe even some strategies they use. But what if you could make one?
+        </h3>
+        <h2>
+          Well, why don't you?
+        </h2>
+        <h3>
+          Make a chat app. Anything can fly, from simple text board sites to massive systems with guilds, group chats, and private rooms. Give it a gimmick, give it something you've always wanted. And in the end, get a raspberry pi to host it on and a domain credit.
+        </h3>
+        
       </main>
     </div>
   );
